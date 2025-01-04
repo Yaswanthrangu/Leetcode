db9470c38f52ae279204b3eb52f9b164f654a88a
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2025-01-04 22:53:25 +0530

    Time: 4 ms (42.55%), Space: 45 MB (7.07%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,22 +1,23 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        int m = nums2.length;
-        int res[] = new int[n];
-        Arrays.fill(res, -1);
-        HashMap <Integer, Integer> mp = new HashMap<>();
-        for(int i = 0; i < n; i++) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        for (int i = 0; i < nums1.length; i++) {
             mp.put(nums1[i], i);
         }
-        for(int i = 0; i < m; i++) {
-            if(mp.containsKey(nums2[i])) {
-                for(int j = i+1; j < m; j++) {
-                    if(nums2[i] < nums2[j]) {
-                        res[mp.get(nums2[i])] = nums2[j];
-                        break;
-                    }
-                }
+
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = -1;
+        }
+        Stack<Integer> st = new Stack<>();
+        for(int i = nums2.length - 1; i >= 0; i--) {
+            while(!st.isEmpty() && nums2[i] >= st.peek()) {
+                st.pop();
             }
+            if(mp.containsKey(nums2[i])) {
+                res[mp.get(nums2[i])] = st.isEmpty() ? -1 : st.peek();
+            }
+            st.push(nums2[i]);
         }
         return res;
     }
