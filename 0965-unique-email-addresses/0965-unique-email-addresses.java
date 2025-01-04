efcdb751502582466eb8270219b569d2f93378eb
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2025-01-04 19:05:20 +0530

    Time: 16 ms (48.15%), Space: 45.4 MB (31.06%) - LeetHub

--- 0965-unique-email-addresses/0965-unique-email-addresses.java
+++ 0965-unique-email-addresses/0965-unique-email-addresses.java
@@ -2,24 +2,12 @@ class Solution {
     public int numUniqueEmails(String[] emails) {
         HashSet<String> st = new HashSet<>();
         for (String str : emails) {
-            StringBuilder sb = new StringBuilder();
-            boolean ignore = false;
-            for (int i = 0; i < str.length(); i++) {
-                char c = str.charAt(i);
-                if (c == '@') {
-                    sb.append(str.substring(i));
-                    break;
-                }
-                if (ignore) {
-                    continue;
-                }
-                if (c == '+') {
-                    ignore = true;
-                } else if (c != '.') {
-                    sb.append(c);
-                }
-            }
-            st.add(sb.toString());
+            String parts[] = str.split("@");
+            String local = parts[0];
+            local = local.replace(".","");
+            local = local.split("\\+")[0];
+            String domain = parts[1];
+            st.add(local+'@'+domain);
         }
         return st.size();
     }
