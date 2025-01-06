64f6010b308a406e119c3ceee4e45bb6ba280b02
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2025-01-06 21:51:22 +0530

    Time: 33 ms (31.77%), Space: 45.8 MB (98.27%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.java
+++ 0817-design-hashmap/0817-design-hashmap.java
@@ -1,29 +1,56 @@
-class MyHashMap {
+public class MyHashMap {
 
-    private int map[]; 
+    private static class ListNode {
+        int key;
+        int value;
+        ListNode next;
+
+        ListNode(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private final ListNode[] map;
 
     public MyHashMap() {
-        map = new int[1000001];
-        Arrays.fill(map, -1);
+        map = new ListNode[10];
+        for (int i = 0; i < map.length; i++) {
+            map[i] = new ListNode(0, 0);
+        }
     }
-    
+
     public void put(int key, int value) {
-        map[key] = value;
+        ListNode cur = map[key % map.length];
+        while (cur.next != null) {
+            if (cur.next.key == key) {
+                cur.next.value = value; 
+                return;
+            }
+            cur = cur.next;
+        }
+        cur.next = new ListNode(key, value);
     }
-    
+
     public int get(int key) {
-        return map[key];
+        ListNode cur = map[key % map.length];
+        while (cur.next != null) {
+            if (cur.next.key == key) {
+                return cur.next.value;
+            }
+            cur = cur.next;
+        }
+        return -1;
     }
-    
+
     public void remove(int key) {
-        map[key] = -1;
+        ListNode cur = map[key % map.length];
+        while (cur.next != null) {
+            if (cur.next.key == key) {
+                cur.next = cur.next.next; 
+                return;
+            }
+            cur = cur.next;
+        }
     }
 }
-
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap obj = new MyHashMap();
- * obj.put(key,value);
- * int param_2 = obj.get(key);
- * obj.remove(key);
- */
