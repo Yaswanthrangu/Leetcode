a46ab8e67aae6efa2b1374deb774966f601e0604
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2025-01-06 17:57:30 +0530

    Time: 1 ms (81.99%), Space: 41.6 MB (39.53%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,25 +1,26 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        char arr1[] = pattern.toCharArray();
         String arr2[] = s.split(" ");
-        int n1 = arr1.length;
+        int n1 = pattern.length();
         int n2 = arr2.length;
         if(n1 != n2) {
             return false;
         }
-        HashMap <Character, String> mp = new HashMap<>();
+        HashMap <Character, String> mp1 = new HashMap<>();
+        HashMap <String, Character> mp2 = new HashMap<>();
         for(int i = 0; i < n1; i++) {
-            if(!mp.containsKey(arr1[i])) {
-                if(mp.containsValue(arr2[i])) {
-                    return false;
-                }
-                mp.put(arr1[i], arr2[i]);
-            }
-            else {
-                if(!mp.get(arr1[i]).equals(arr2[i])) {
+            if(mp1.containsKey(pattern.charAt(i))) {
+                if(!mp1.get(pattern.charAt(i)).equals(arr2[i])) {
                     return false;
                 }
             }
+            mp1.put(pattern.charAt(i), arr2[i]);
+            if(mp2.containsKey(arr2[i])) {
+                if(!mp2.get(arr2[i]).equals(pattern.charAt(i))) {
+                    return false;
+                }
+            }
+            mp2.put(arr2[i], pattern.charAt(i));
         }
         return true;
     }
