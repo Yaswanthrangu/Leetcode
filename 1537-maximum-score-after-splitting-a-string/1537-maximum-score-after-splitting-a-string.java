0ad9633463d41e75f183aae562dee6304053e5e9
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2025-01-10 22:34:41 +0530

    Time: 2 ms (31.77%), Space: 41.8 MB (28.31%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
@@ -1,19 +1,32 @@
 class Solution {
     public int maxScore(String s) {
+        int n = s.length();
+        int[] leftZero = new int[n];
+        int[] rightOne = new int[n];
+
+        if (s.charAt(0) == '0') {
+            leftZero[0] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            leftZero[i] = leftZero[i - 1];
+            if (s.charAt(i) == '0') {
+                leftZero[i]++;
+            }
+        }
+
+        if (s.charAt(n - 1) == '1') {
+            rightOne[n - 1] = 1;
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            rightOne[i] = rightOne[i + 1];
+            if (s.charAt(i) == '1') {
+                rightOne[i]++;
+            }
+        }
+
         int res = 0;
-        for(int bp = 0; bp < s.length() - 1; bp++) {
-            int temp = 0;
-            for(int left = 0; left <= bp; left++) {
-                if(s.charAt(left) == '0') {
-                    temp++;
-                }
-            }
-            for(int right = bp + 1; right < s.length(); right++) {
-                if(s.charAt(right) == '1') {
-                    temp++;
-                }
-            }
-            res = Math.max(res, temp);
+        for (int i = 1; i < n; i++) {
+            res = Math.max(res, leftZero[i - 1] + rightOne[i]);
         }
         return res;
     }
