0e4a41e15894b71175775dc70ac2ced41f7cd9d2
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2025-01-13 22:12:45 +0530

    Time: 4 ms (71.94%), Space: 42.5 MB (55.51%) - LeetHub

--- 1884-minimum-changes-to-make-alternating-binary-string/1884-minimum-changes-to-make-alternating-binary-string.java
+++ 1884-minimum-changes-to-make-alternating-binary-string/1884-minimum-changes-to-make-alternating-binary-string.java
@@ -1,25 +1,18 @@
 class Solution {
     public int minOperations(String s) {
         int start_with_zero = 0;   // 010101...
-        int start_with_one = 0;    // 101010...
         for(int i = 0; i < s.length(); i++) {
             if(i % 2 == 0) {  // even position
-                if(s.charAt(i) == '0') {
-                    start_with_one++;
-                }
-                else {
+                if(s.charAt(i) == '1') {
                     start_with_zero++;
                 }
             }
             else {  // odd position
-                if(s.charAt(i) == '1') {
-                    start_with_one++;
-                }
-                else {
+                if(s.charAt(i) == '0') {
                     start_with_zero++;
                 }
             }
         }
-        return Math.min(start_with_zero, start_with_one);
+        return Math.min(start_with_zero, s.length() - start_with_zero);
     }
 }
