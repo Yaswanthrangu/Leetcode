0ef34fdabc24c5d90b40938a8286c8401d9fc4b5
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2025-01-19 22:24:58 +0530

    Time: 0 ms (100%), Space: 41.6 MB (24.66%) - LeetHub

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,35 +1,22 @@
 class Solution {
     public int specialArray(int[] nums) {
-        Arrays.sort(nums);
         int n = nums.length;
-        int x_low = 0, x_high = n;
-        while(x_low <= x_high) {
-            int x_mid = x_low + (x_high - x_low) / 2;
-            int idx = lower_bound(nums, x_mid);
-            if(n - idx == x_mid) {
-                return x_mid;
-            }
-            else if(n - idx > x_mid) {
-                x_low = x_mid + 1;
+        int freqArr[] = new int[n + 1];
+        for(int num : nums) {
+            if(num > n) {
+                freqArr[n]++;
             }
             else {
-                x_high = x_mid - 1;
+                freqArr[num]++;
+            }
+        }
+        int cumulative_sum = 0;
+        for(int i = n; i >= 0; i--) {
+            cumulative_sum += freqArr[i];
+            if(cumulative_sum == i) {
+                return i;
             }
         }
         return -1;
-    }
-    public static int lower_bound(int nums[], int ele) {
-        int low = 0;
-        int high = nums.length;
-        while(low < high) {
-            int mid = low + (high - low) / 2;
-            if(nums[mid] >= ele) {
-                high = mid;
-            }
-            else {
-                low = mid + 1;
-            }
-        }
-        return low;
     }
 }
