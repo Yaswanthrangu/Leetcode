62410774d8640748b17d029efb19458e74c45243
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2025-01-19 20:30:25 +0530

    Time: 2 ms (48.78%), Space: 41.7 MB (17.16%) - LeetHub

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,17 +1,27 @@
 class Solution {
     public int specialArray(int[] nums) {
+        Arrays.sort(nums);
         int n = nums.length;
         for(int x = 0; x <= n; x++) {
-            int cnt = 0;
-            for(int i = 0; i < n; i++) {
-                if(nums[i] >= x) {
-                    cnt++;
-                }
-            }
-            if(cnt == x) {
-                return cnt;
+            int idx = lower_bound(nums, x);
+            if(n - idx == x) {
+                return x;
             }
         }
         return -1;
+    }
+    public static int lower_bound(int nums[], int ele) {
+        int low = 0;
+        int high = nums.length;
+        while(low < high) {
+            int mid = low + (high - low) / 2;
+            if(nums[mid] >= ele) {
+                high = mid;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return low;
     }
 }
