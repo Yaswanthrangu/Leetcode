75357cdbf4040e8865424839bd272a19c6f7ef25
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2025-01-23 17:37:04 +0530

    Time: 0 ms (100%), Space: 40.9 MB (86.27%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rob(int[] nums) {
+        int dp[] = new int[101];
+        Arrays.fill(dp, -1);
+        return solve(nums, 0, dp);
+    }
+    public static int solve(int nums[], int idx, int dp[]) {
+        int n = nums.length;
+        if(idx >= n) {
+            return 0;
+        }
+        if(dp[idx] != -1) {
+            return dp[idx];
+        }
+        int steal = nums[idx] + solve(nums, idx + 2, dp);
+        int skip = solve(nums, idx + 1, dp);
+        return dp[idx] = Math.max(steal, skip);
+    }
+}
