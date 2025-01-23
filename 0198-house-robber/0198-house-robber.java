f2367f191a3b9b8b9511d92ed8b9428ab0b360b5
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2025-01-23 19:08:01 +0530

    Time: 0 ms (100%), Space: 40.8 MB (86.27%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,23 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int prevPrev = 0;
-        int prev = nums[0];
-        for(int i = 2; i <= n; i++) {  // i = 2 resembles nums[1]
-            int steal = nums[i - 1] + prevPrev;
-            int skip = prev;
-            int curr = Math.max(steal, skip);
-            prevPrev = prev;
-            prev = curr;
+        int res1 = 0;
+        int res2 = 0;
+        int dp[] = new int[n+1];
+        dp[0] = 0;
+        for(int i = 1; i <= n; i++) {
+            int steal = nums[i - 1] + ((i - 2 >= 0) ? dp[i - 2] : 0);
+            int skip = dp[i - 1];
+            dp[i] = Math.max(steal, skip);
         }
-        return prev;
+        res1 = dp[n];
+        Arrays.fill(dp, 0);
+        for(int i = 2; i <= n - 1; i++) {
+            int steal = nums[i - 1] + dp[i - 2];
+            int skip = dp[i - 1];
+            dp[i] = Math.max(steal, skip);
+        }
+        res2 = dp[n];
+        return Math.max(res1, res2);
     }
 }
