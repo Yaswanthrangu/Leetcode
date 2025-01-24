515522905a2652f222ab396d7be70b6214e6bdc6
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2025-01-24 13:46:46 +0530

    Time: 172 ms (29.27%), Space: 46.5 MB (13.27%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,25 +1,29 @@
 class Solution {
     public String longestPalindrome(String s) {
         int n = s.length();
-        int start_point = 0;
-        int max_len = 0;
-        int dp[][] = new int[n][n];
-        for(int i = 0; i < n; i++) {
-            for(int j = i; j < n; j++) {
-                if(isPalindrome(s, i, j, dp) == 1 && (j - i + 1 > max_len)) {
-                    start_point = i;
-                    max_len = j - i + 1;
+        boolean dp[][] = new boolean[n][n];
+        int max_len = Integer.MIN_VALUE;
+        int start_point = Integer.MIN_VALUE;
+        for(int len = 1; len <= n; len++) {
+            for(int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+                if(i == j) {
+                    dp[i][j] = true;
+                }
+                else if(i + 1 == j) {
+                    dp[i][j] = (s.charAt(i) == s.charAt(j));
+                }
+                else {
+                    dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i+1][j-1] == true);
+                }
+                if(dp[i][j] == true) {
+                    if(max_len < j - i + 1) {
+                        start_point = i;
+                        max_len = j - i + 1;
+                    }
                 }
             }
         }
         return s.substring(start_point, start_point + max_len);
-    }
-    public static int isPalindrome(String s, int i, int j, int dp[][]) {
-        if(i >= j) return 1;
-        if(dp[i][j] != 0) return dp[i][j];
-        if(s.charAt(i) == s.charAt(j)) {
-            return dp[i][j] = isPalindrome(s, i + 1, j - 1, dp);
-        }
-        return dp[i][j] = 0;
     }
 }
