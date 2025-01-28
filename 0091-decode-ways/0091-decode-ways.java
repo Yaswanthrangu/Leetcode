d36cae7540521988f0f4624442288603b0b00889
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2025-01-28 16:21:08 +0530

    Time: 0 ms (100%), Space: 41.9 MB (63.45%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,21 +1,21 @@
-class Solution {
+public class Solution {
     public int numDecodings(String s) {
         int n = s.length();
-        int dp[] = new int[n + 1];
-        dp[n] = 1;
-        for(int i = n - 1; i >= 0; i--) {
-            if(s.charAt(i) == '0') {
-                dp[i] = 0;
+        int[] dp = new int[n + 1];
+        dp[0] = 1; // An empty string has one way to decode
+
+        for (int i = 1; i <= n; i++) {
+            if (s.charAt(i - 1) != '0') { // Single-character decoding
+                dp[i] = dp[i - 1];
             }
-            else {
-                dp[i] = dp[i+1];
-                if(i + 1 < n) {
-                    if(s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i+1) <= '6')) {
-                        dp[i] += dp[i+2];
-                    }
+            if (i > 1) { // Two-character decoding
+                char first = s.charAt(i - 2);
+                char second = s.charAt(i - 1);
+                if (first == '1' || (first == '2' && second >= '0' && second <= '6')) {
+                    dp[i] += dp[i - 2];
                 }
             }
         }
-        return dp[0];
+        return dp[n];
     }
 }
