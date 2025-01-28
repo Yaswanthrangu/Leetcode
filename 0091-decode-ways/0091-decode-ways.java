e134bc9db6c36adcd01c53a49b8392a73bd1161c
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2025-01-28 15:38:00 +0530

    Time: 0 ms (100%), Space: 41.9 MB (63.45%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,27 +1,21 @@
 class Solution {
     public int numDecodings(String s) {
-        int dp[] = new int[s.length() + 1];
-        Arrays.fill(dp, -1);
         int n = s.length();
-        return solve(0, s, n, dp);
-    }
-    public static int solve(int idx, String s, int n, int dp[]) {
-        if(dp[idx] != -1) {
-            return dp[idx];
-        }
-        if(idx >= n) {
-            return dp[idx] = 1;
-        }
-        if(s.charAt(idx) == '0') {
-            return dp[idx] = 0;
-        }
-        int only_ith_place_split = solve(idx + 1, s, n, dp);
-        int two_places_from_i = 0;
-        if(idx + 1 < n) {
-            if(s.charAt(idx) == '1' || (s.charAt(idx+1) <='6' && s.charAt(idx) == '2')) {
-                two_places_from_i = solve(idx + 2, s, n, dp);
+        int dp[] = new int[n + 1];
+        dp[n] = 1;
+        for(int i = n - 1; i >= 0; i--) {
+            if(s.charAt(i) == '0') {
+                dp[i] = 0;
+            }
+            else {
+                dp[i] = dp[i+1];
+                if(i + 1 < n) {
+                    if(s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i+1) <= '6')) {
+                        dp[i] += dp[i+2];
+                    }
+                }
             }
         }
-        return dp[idx] = only_ith_place_split + two_places_from_i;
+        return dp[0];
     }
 }
