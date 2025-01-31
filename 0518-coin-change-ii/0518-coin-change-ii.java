cc8c9705d6cd97326ff7f2a4b9accd49ad56e9d0
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2025-01-31 19:44:58 +0530

    Time: 8 ms (70.98%), Space: 49 MB (54.06%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,14 +1,19 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int n = coins.length;
-        int dp[] = new int[amount + 1];
-        dp[0] = 1;
-        for(int i = 0; i < coins.length; i++) {
-            // if coins[i] = 5 loop from that idx because j - amount will give neg idx if starts from 0
-            for(int j = coins[i]; j < dp.length; j++) {
-                dp[j] += dp[j - coins[i]];
+        int dp[][] = new int[coins.length + 1][amount + 1];
+        for(int i = 0; i < coins.length + 1; i++) {
+            dp[i][0] = 1;
+        }
+        for(int i = 1; i < coins.length + 1; i++) {
+            for(int j = 1; j < amount + 1; j++) {
+                if(coins[i-1] <= j) {
+                    dp[i][j] = dp[i][j - coins[i-1]] + dp[i-1][j];
+                }
+                else {
+                    dp[i][j] = dp[i-1][j];
+                }
             }
         }
-        return dp[amount];
+        return dp[coins.length][amount];
     }
 }
