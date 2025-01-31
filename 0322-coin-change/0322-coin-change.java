ebd43d43b61274c6e2284738952b72579a88d608
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2025-01-31 22:37:05 +0530

    Time: 11 ms (95.99%), Space: 44.6 MB (58.22%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,22 +1,13 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int dp[][] = new int[coins.length + 1][amount + 1];
-        for(int i = 0; i < coins.length; i++) {
-            dp[i][0] = 0;
-        }
-        for(int j = 1; j < amount + 1; j++) {
-            dp[0][j] = Integer.MAX_VALUE - 1;
-        }
-        for(int i = 1; i < coins.length + 1; i++) {
-            for(int j = 1; j < amount + 1; j++) {
-                if(coins[i-1] <= j) {
-                    dp[i][j] = Math.min(dp[i-1][j] , 1+dp[i][j-coins[i-1]]);
-                }
-                else {
-                    dp[i][j] = dp[i-1][j];
-                }
+        int dp[] = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE - 1);
+        dp[0] = 0;
+        for (int coin : coins) {
+            for (int j = coin; j <= amount; j++) {
+                dp[j] = Math.min(dp[j], 1 + dp[j - coin]);
             }
         }
-        return dp[coins.length][amount] == (Integer.MAX_VALUE - 1) ? -1 : dp[coins.length][amount];
+        return dp[amount] == Integer.MAX_VALUE - 1 ? -1 : dp[amount];
     }
 }
