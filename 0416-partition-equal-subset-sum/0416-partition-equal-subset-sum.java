0275dcfa4e9cda49f126258b9001f27a0d2f44cf
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2025-02-01 11:47:07 +0530

    Time: 43 ms (53.76%), Space: 46.7 MB (56.67%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -6,16 +6,20 @@ class Solution {
         }
         if(total_sum % 2 != 0) return false;
         int req_sum = total_sum / 2;
-        Boolean dp[][] = new Boolean[nums.length + 1][req_sum + 1];
-        return solve(nums.length - 1, nums, req_sum, 0, dp);
-    }
-    public static boolean solve(int idx, int nums[], int target, int curr_sum, Boolean dp[][]) {
-        if(curr_sum == target) return true;
-        if(idx < 0) return false;
-        if(curr_sum > target) return false;
-        if(dp[idx][curr_sum] != null) return dp[idx][curr_sum];
-        Boolean include = solve(idx - 1, nums, target, curr_sum + nums[idx], dp);
-        Boolean exclude = solve(idx - 1, nums, target, curr_sum, dp);
-        return dp[idx][curr_sum] = include || exclude;
+        boolean dp[][] = new boolean[nums.length + 1][req_sum + 1];
+        for(int i = 0; i < nums.length + 1; i++) {
+            dp[i][0] = true;
+        }
+        for(int i = 1; i < nums.length + 1; i++) {
+            for(int j = 1; j < req_sum + 1; j++) {
+                if(nums[i-1] <= j) {
+                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                }
+                else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[nums.length][req_sum];
     }
 }
