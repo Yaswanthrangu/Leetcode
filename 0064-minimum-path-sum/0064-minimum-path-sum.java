dd5cf47e095e8aeabd983b09e40f352a87b5c739
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2025-02-09 19:49:54 +0530

    Time: 4 ms (62.8%), Space: 47.6 MB (69.18%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,28 +1,20 @@
 class Solution {
     public int minPathSum(int[][] grid) {
         int dp[][] = new int[grid.length][grid[0].length];
-        for(int i = 0; i < grid.length; i++) {
-            for(int j = 0; j < grid[0].length; j++) {
-                dp[i][j] = -1;
+        dp[0][0] = grid[0][0];
+        for(int i = 1; i < grid.length; i++) {
+            // As we traversing till row, need to put i at 1st brackett
+            dp[i][0] = grid[i][0] + dp[i-1][0];
+        }
+        for(int i = 1; i < grid[0].length; i++) {
+            // As we traversing till column, need to put i at 2nd brackett
+            dp[0][i] = grid[0][i] + dp[0][i-1];
+        }
+        for(int i = 1; i < grid.length; i++) {
+            for(int j = 1; j < grid[0].length; j++) {
+                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
             }
         }
-        return solve(0, 0, grid.length, grid[0].length, grid, dp);
-    }
-    public static int solve(int i, int j, int m, int n, int grid[][], int dp[][]) {
-        if(i == m - 1 && j == n - 1) {
-            return grid[i][j];
-        }
-        if(dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        if(i == m - 1) {
-            return dp[i][j] = grid[i][j] + solve(i, j+1, m, n, grid, dp);
-        }
-        else if(j == n - 1) {
-            return dp[i][j] = grid[i][j] + solve(i+1, j, m, n, grid, dp);
-        }
-        else {
-            return dp[i][j] = grid[i][j] + Math.min(solve(i, j+1, m, n, grid, dp), solve(i+1, j, m, n, grid, dp));
-        }
+        return dp[grid.length-1][grid[0].length-1];
     }
 }
