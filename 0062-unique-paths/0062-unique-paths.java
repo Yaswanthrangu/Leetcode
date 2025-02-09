8910e81c6d3650a3f20e2b0aeced313877d59bd3
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2025-02-09 11:15:33 +0530

    Time: 0 ms (100%), Space: 40.2 MB (87.1%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int dp[][] = new int[m+1][n+1];
+        for(int i = 0; i < m + 1; i++) {
+            for(int j = 0; j < n + 1; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        return solve(0,0, m, n, dp);
+    }
+    public static int solve(int i, int j, int m, int n, int dp[][]) {
+        // reached destination
+        if(i == m - 1 && j == n - 1) return 1;
+        // out of bounds
+        if(i < 0 || i >= m || j < 0 || j >= n) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        int down = solve(i+1, j, m, n, dp);
+        int right = solve(i, j+1, m, n, dp);
+        return dp[i][j] = down + right;
+    }
+}
