d27dc1890b4f1db7fdb62d92bdaf22947ab42656
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2025-02-11 11:36:10 +0530

    Time: 12 ms (80.2%), Space: 42.4 MB (84.11%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
@@ -1,26 +1,24 @@
 class Solution {
     public int minCost(int n, int[] cuts) {
-        Arrays.sort(cuts);
-        int dp[][] = new int[cuts.length][cuts.length];
-        for(int i = 0; i < cuts.length; i++) {
-            for(int j = 0; j < cuts.length; j++) {
-                dp[i][j] = -1;
+        int m = cuts.length;
+        int arr[] = new int[m+2];
+        arr[0] = 0;
+        arr[m+1] = n;
+        for(int i = 1; i <= m; i++) {
+            arr[i] = cuts[i-1];
+        }
+        int dp[][] = new int[m+2][m+2];
+        Arrays.sort(arr);
+        for(int len = 2; len <= m + 1; len++) {
+            for(int row = 0; row <= m+1-len; row++) {
+                int col = len + row;
+                dp[row][col] = Integer.MAX_VALUE; // 0 overiding with infinity
+                for(int k = row+1; k <= col-1; k++) {
+                    int totalCost = dp[row][k] + dp[k][col] + (arr[col] - arr[row]);
+                    dp[row][col] = Math.min(totalCost, dp[row][col]);
+                }
             }
         }
-        return solve(0, n, cuts, 0, cuts.length-1, dp);
-    }
-    public static int solve(int start_stick, int end_stick, int cuts[], int leftCut, int rightCut, int dp[][]) {
-        int res = Integer.MAX_VALUE;
-        if(leftCut > rightCut) {
-            return 0;
-        }
-        if(dp[leftCut][rightCut] != -1) return dp[leftCut][rightCut];
-        for(int i = leftCut; i <= rightCut; i++) {
-            int left_cut_cost = solve(start_stick, cuts[i], cuts, leftCut, i-1, dp);
-            int right_cut_cost = solve(cuts[i], end_stick, cuts, i+1, rightCut, dp);
-            int total_cost = (end_stick - start_stick) + left_cut_cost + right_cut_cost;
-            res = Math.min(res, total_cost);
-        }
-        return dp[leftCut][rightCut] = res;
+        return dp[0][m+1];
     }
 }
