48a83e140a99f3d24b698f97a188b7a7b5769eeb
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2025-02-14 16:11:38 +0530

    Time: 10 ms (15.26%), Space: 45 MB (16.29%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -2,17 +2,16 @@ class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         int n = s.length();
         HashSet <String> st = new HashSet<>(wordDict);
-        
-        boolean dp[] = new boolean[n+1];
-        dp[0] = true;
-        
-        for(int end = 1; end <= n; end++) {
-            for(int stIdx = 0; stIdx < end; stIdx++) {
-                if(dp[stIdx] && st.contains(s.substring(stIdx , end))) {
+        boolean dp[] = new boolean[n];
+        for(int start = 0; start < n; start++) { // n - 1 index will be considered at last
+            for(int end = start; end < n; end++) { // like a sub array
+                String formed = s.substring(start, end+1); // end + 1 (0,1)
+                // if end = n - 1 then to calculate the substring  = (n - 1, n)
+                if((start == 0 || dp[start-1]) && st.contains(formed)) {
                     dp[end] = true;
                 }
             }
         }
-        return dp[n];
+        return dp[n-1];
     }
 }
