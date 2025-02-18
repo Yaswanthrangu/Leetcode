728526616d8cd3651a399a74944e108638726b4c
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2025-02-18 22:31:57 +0530

    Time: 105 ms (54.22%), Space: 45.7 MB (7.41%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,14 +1,18 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return solve(nums, 0, 0, target);
+        HashMap <String, Integer> mp = new HashMap<>();
+        return solve(nums, 0, 0, target, mp);
     }
-    public static int solve(int nums[], int idx, int currSum, int target) {
+    public static int solve(int nums[], int idx, int currSum, int target, HashMap <String, Integer> mp) {
         if(idx == nums.length) {
             if(currSum == target) return 1;
             else return 0;
         }
-        int plus = solve(nums, idx + 1, currSum + nums[idx], target);
-        int minus = solve(nums, idx + 1, currSum - nums[idx], target);
-        return plus + minus;
+        String key = idx + "_" + currSum;
+        if(mp.containsKey(key)) return mp.get(key);
+        int plus = solve(nums, idx + 1, currSum + nums[idx], target, mp);
+        int minus = solve(nums, idx + 1, currSum - nums[idx], target, mp);
+        mp.put(key, plus + minus);
+        return mp.get(key);
     }
 }
