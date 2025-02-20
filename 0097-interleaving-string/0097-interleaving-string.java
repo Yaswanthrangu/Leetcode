08c8eb2d11643840d8bb3c61df393ae9f0889ee3
Rangu Yaswanth <125751896+Yaswanthrangu@users.noreply.github.com>
2025-02-20 22:46:33 +0530

    Time: 0 ms (100%), Space: 42 MB (44.46%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -3,26 +3,26 @@ class Solution {
         int n = s1.length();
         int m = s2.length();
         int l = s3.length();
-        Boolean dp[][][] = new Boolean[n+1][m+1][l+1];
+        Boolean dp[][] = new Boolean[n+1][m+1];
         if(n + m != l) return false;
-        return solve(0, 0, 0, s1, s2, s3, n, m, l, dp);
+        return solve(0, 0, s1, s2, s3, n, m, l, dp);
     }
-    public static boolean solve(int i, int j, int k, String s1, String s2, String s3, int n, int m, int l, Boolean dp[][][]) {
-        if(i == n && j == m && k == l) {
+    public static boolean solve(int i, int j, String s1, String s2, String s3, int n, int m, int l, Boolean dp[][]) {
+        if(i == n && j == m) {
             return true;
         }
-        if(k >= l) {
+        if(i+j >= l) {
             return false;
         }
         boolean result = false;
-        if(dp[i][j][k] != null) return dp[i][j][k];
-        if(i < n && s1.charAt(i) == s3.charAt(k)) {
-            result = solve(i+1, j, k+1, s1, s2, s3, n, m, l, dp);
+        if(dp[i][j] != null) return dp[i][j];
+        if(i < n && s1.charAt(i) == s3.charAt(i+j)) {
+            result = solve(i+1, j, s1, s2, s3, n, m, l, dp);
         }
-        if(result) return true;
-        if(j < m && s2.charAt(j) == s3.charAt(k)) {
-            result = solve(i, j+1, k+1, s1, s2, s3, n, m, l, dp);
+        if(result) return dp[i][j] = true;
+        if(j < m && s2.charAt(j) == s3.charAt(i+j)) {
+            result = solve(i, j+1, s1, s2, s3, n, m, l, dp);
         }
-        return dp[i][j][k] = result;
+        return dp[i][j] = result;
     }
 }
